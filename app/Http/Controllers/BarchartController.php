04e95b4445c815318d79152543ed5758aeb92981
Mohammad Ali Khan <khan@nochallenge.net>
2023-01-17 21:09:35 +0600

    initial commit

---
+++ app/Http/Controllers/BarchartController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class BarchartController extends Controller
+{
+    public function barchart(Request $request)
+    {
+    	$fruit = Product::where('product_type','fruit')->get();
+    	$veg = Product::where('product_type','vegitable')->get();
+    	$grains = Product::where('product_type','grains')->get();
+    	$fruit_count = count($fruit);    	
+    	$veg_count = count($veg);
+    	$grains_count = count($grains);
+    	
+    	return view('barchart',compact('fruit_count','veg_count','grains_count'));
+    }
+}
